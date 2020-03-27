8c878f8eee446b65ad6b712e5b1573e3aff3545e
OroDev <oro91dev@gmail.com>
2020-03-27 20:50:18 +0100

    Create 39 dateFormatting.sql

---
+++ 39 dateFormatting.sql
@@ -0,0 +1,10 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE dateFormatting()
+BEGIN
+	SELECT DATE_FORMAT(date_str,'%Y-%m-%d') AS date_iso
+    FROM documents
+    ORDER BY
+    id;
+    
+    /* STR_TO_DATE(date_str,'%Y-%m-%d')*/
+END
