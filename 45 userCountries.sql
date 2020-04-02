39f6725d33e1502574dba7ce770d1791c54064e6
OroDev <oro91dev@gmail.com>
2020-04-02 21:11:15 +0200

    Create 45 userCountries.sql

---
+++ 45 userCountries.sql
@@ -0,0 +1,9 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE userCountries()
+BEGIN
+	SELECT u.id, IFNULL(c.country, "unknown") AS country
+    FROM users u 
+    LEFT JOIN cities c
+    ON u.city = c.city
+    ORDER BY u.id;
+END
