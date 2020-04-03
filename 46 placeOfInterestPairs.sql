1498484b912ee07f958e5568176c4f5c3a26d5e3
OroDev <oro91dev@gmail.com>
2020-04-03 14:26:59 +0200

    Create 46 placeOfInterestPairs.sql

---
+++ 46 placeOfInterestPairs.sql
@@ -0,0 +1,9 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE placesOfInterestPairs()
+BEGIN
+	SELECT a.name as place1, b.name as place2
+    FROM sights a
+    JOIN sights b ON(a.name < b.name)
+    WHERE  ST_DISTANCE(POINT(a.`x`,a.`y`), POINT(b.`x`, b.`y`)) < 5
+    ORDER BY place1, place2;
+END
