b9eec8a9a6b95a3eb3eeb9855adf839dcb2c739e
OroDev <oro91dev@gmail.com>
2020-04-13 11:29:04 +0200

    Create 55 studentsInClubs.sql

---
+++ 55 studentsInClubs.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE studentsInClubs()
+    SELECT * FROM students
+    WHERE EXISTS (
+        SELECT *
+        FROM clubs
+        WHERE students.club_id = clubs.id
+        
+    )
+    ORDER BY students.id;
