d7ae971322a799478117130ee2600e7edd560ff2
OroDev <oro91dev@gmail.com>
2020-04-16 19:31:34 +0200

    Create 58 closestCells.sql

---
+++ 58 closestCells.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE closestCells()
+BEGIN
+	SELECT p1.id id1, (
+		SELECT id FROM positions p2
+		WHERE p1.id != p2.id
+		ORDER BY power(p1.x-p2.x,2) + power(p1.y-p2.y,2) LIMIT 1) AS id2
+	FROM positions p1
+    ORDER BY p1.id ASC;
+END
